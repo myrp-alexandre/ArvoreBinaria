88d6cf71e3ab9adc0624e4d0f6bc7e32dbfee6cc
Flp <felipeputsch@unifebe.edu.br>
2019-02-27 21:46:03 -0300

    Adicionado

---
+++ ArvoreBinaria/Node.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ArvoreBinaria
+{
+    public class Node
+    {
+        public long Valor { get; set; }
+        public Node Esquerda { get; set; }//menor
+        public Node Direita { get; set; }//maior
+
+        public Node(long novoValor)
+        {
+            Valor = novoValor;
+        }
+
+        public bool Insere(long valorNovo)
+        {
+            if (valorNovo == Valor)
+            {
+                Console.WriteLine("Valor ja informado!");
+            }
+            else
+            {
+                if (valorNovo < Valor)
+                {
+                    if (Esquerda == null)
+                    {
+                        Esquerda = new Node(valorNovo);
+                        Console.WriteLine("Inserido " + valorNovo + " a esquerda!");
+                    }
+                    else
+                    {
+                        return Esquerda.Insere(valorNovo);
+                    }
+                }
+                else
+                {
+                    if (Direita == null)
+                    {
+                        Direita = new Node(valorNovo);
+                        Console.WriteLine("Inserido " + valorNovo + " a direita!");
+                    }
+                    else
+                    {
+                        return Direita.Insere(valorNovo);
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ArvoreBinaria/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArvoreBinaria
+{
+    internal class Program
+    {
+        private static ArvoreBinaria Arvore { get; set; }
+
+        private static void Main(string[] args)
+        {
+            Arvore = new ArvoreBinaria();
+
+            while (true)
+            {
+                Console.WriteLine("Informe o Valor! Informe N para parar!");
+                var comando = Console.ReadLine();
+                if (comando.ToUpper() == "N")
+                {
+                    break;
+                }
+
+                Arvore.Insere(long.Parse(comando));
+            }
+            Console.Clear();
+            Console.WriteLine("Qual número você deseja procurar?");
+            var procurado = Console.ReadLine();
+            Arvore.Percorrer(long.Parse(procurado));
+
+            Console.ReadKey();
+        }
+    }
+}
